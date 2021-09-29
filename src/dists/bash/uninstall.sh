aadcbbfc0ea01925cc753f00ed3e80753bb71c71
Roberto Pompermaier <robypomper@johnosproject.com>
2021-09-29 12:02:34 +0200

    fix(dist): Fixed wrong PRE startup reference in uninstall.sh JOD Distribution Script

--- src/dists/bash/uninstall.sh
+++ src/dists/bash/uninstall.sh
@@ -65,7 +65,7 @@ if [ "$STATUS_INSTALL" = "Not Installed" ]; then
 fi
 
 logInf "Execute pre-uninstall.sh..."
-if [ -f "$JOD_DIR/scripts/pre-startup.sh" ]; then
+if [ -f "$JOD_DIR/scripts/pre-uninstall.sh" ]; then
   execScriptCommand $JOD_DIR/scripts/pre-uninstall.sh || ([ "$?" -gt "0" ] &&
     logWar "Error executing PRE uninstall script, exit $?" && exit $? ||
     logWar "Error executing PRE uninstall script, continue $?")
