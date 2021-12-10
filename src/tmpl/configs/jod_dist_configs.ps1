10d432e0ebc850676e37b66a2effe8ee7b30496a
Roberto Pompermaier <robypomper@johnosproject.com>
2021-12-10 16:44:01 +0100

    refactor(tmpl): Updated JOD Distribution TEMPLATE default configs to JOD version 2.2.2
    (cherry picked from commit d5bf566f3b6e0a92d04fd9bbbbeb0a31b64f865f)

--- src/tmpl/configs/jod_dist_configs.ps1
+++ src/tmpl/configs/jod_dist_configs.ps1
@@ -91,7 +91,7 @@ $global:DIST_JCP_ENV = "local"
 # JOD Agent version to include in the generated distribution
 # JOD agent's and his dependencies will be first downloaded from central maven
 # repository, if not available, then will be copied from local maven repository.
-$global:DIST_JOD_VER = "2.2.1"
+$global:DIST_JOD_VER = "2.2.2"
 
 # JOD Object's name
 # A string used as JOD object's name. All instances of current JOD Distribution
