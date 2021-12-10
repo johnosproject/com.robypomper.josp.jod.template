d4a5e6a5c2af3b6b940cb49629a6e72fe68bacf6
Roberto Pompermaier <robypomper@johnosproject.com>
2021-12-10 13:08:37 +0100

    feat(tmpl): Added errors constants

--- src/tmpl/ps/jod_tmpl/include.ps1
+++ src/tmpl/ps/jod_tmpl/include.ps1
@@ -24,8 +24,8 @@
 # Version:  1.0.1
 ################################################################################
 
+param ([Parameter(Mandatory)] $JOD_DIR)
+
 $JOD_TMPL_VERSION = "1.0.1"
 
-$JOD_DIR=$1
-
-#."$JOD_DIR/scripts/libs/{LIB_ABC}.sh"
+.$JOD_DIR/scripts/jod_tmpl/errors.ps1
